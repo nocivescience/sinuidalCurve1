1e36f7c8686d83a791ed847aee3fee201fd01a9e
nocive.science <nocive.science@gmail.com>
2024-02-17 18:03:04 -0300

    terminado

--- script.js
+++ script.js
@@ -1,58 +1,69 @@
 // Obtén el elemento canvas y su contexto
 const canvas = document.getElementById('curva');
 const ctx = canvas.getContext('2d');
+let face=0;
 
 // creacion de la clase
 class Curva{
-    constructor(amplitud, periodo, fase, desplazamiento){
-        this.amplitud = amplitud;
-        this.periodo = periodo;
-        this.fase = fase;
-        this.desplazamiento = desplazamiento;
-    }
-    // Define la función sinusoidal
-    sinWave(x) {
-        return this.amplitud * Math.sin(this.periodo * (x - this.fase)) + this.desplazamiento;
+    constructor(funcion, dx = 0){
+        this.funcion = funcion;
+        this.dx = dx;
     }
     dibujar(){
         ctx.beginPath();
-        for(let x = 0; x < canvas.width; x++) {
-            let y = this.sinWave(x);
+        for (let x = 0; x < canvas.width; x++) {
+            let y = this.funcion(x, this.dx);
             ctx.lineTo(x, y);
         }
         ctx.stroke();
-    };
-}
-
-let curvas=[];
-
-function linSpace(start, stop, num){
-    let arr = [];
-    let step = (stop-start) / (num-1);
-    for(let i = 0; i < num; i++){
-        arr.push(start + (step * i));
     }
-    return arr;
+    incrementarFase(){
+        this.dx += 0.01;
+    }
 }
+
+const funcionesSinuidales = [
+    (x,dx) => 100 * Math.sin(.05*x+dx)+300,
+    (x,dx) => 100 * Math.cos(.01*x+dx)+300+100*Math.sin(.05*x+dx),
+    (x,dx) => 100 * Math.sin(.03*x+dx)+300+100*Math.cos(.02*x+dx),
+    (x,dx) => 100 * Math.cos(.06*x+dx)+300+100*Math.sin(.02*x+dx),
+    (x,dx) => 100 * Math.sin(.04*x+dx)+300+100*Math.cos(.07*x+dx),
+    (x,dx) => 100 * Math.cos(.08*x+dx)+100+100*Math.sin(.01*x+dx),
+    (x,dx) => 100 * Math.sin(.09*x+dx)+300+100*Math.cos(.09*x+dx),
+    (x,dx) => 100 * Math.sin(.07*x+dx)+300+100*Math.cos(.01*x+dx)+100*Math.sin(.07*x+dx),
+    (x,dx) => 100 * Math.cos(.04*x+dx)+300+100*Math.sin(.03*x+dx)+100*Math.cos(.04*x+dx),
+    (x,dx) => 100 * Math.sin(.02*x+dx)+300+100*Math.cos(.06*x+dx)+100*Math.sin(.02*x+dx),
+    (x,dx) => 100 * Math.cos(.03*x+dx)+300+100*Math.sin(.09*x+dx)+100*Math.cos(.03*x+dx),
+    (x,dx) => 100 * Math.sin(.05*x+dx)+300+100*Math.cos(.08*x+dx)+100*Math.sin(.05*x+dx),
+    (x,dx) => 100 * Math.cos(.01*x+dx)+300+100*Math.sin(.08*x+dx)+100*Math.cos(.02*x+dx)+40*Math.sin(.05*x+dx),
+    (x,dx) => 100 * Math.sin(.09*x+dx)+300+100*Math.cos(.02*x+dx)+100*Math.sin(.09*x+dx)+40*Math.cos(.05*x+dx),
+    (x,dx) => 100 * Math.cos(.01*x+dx)+300+100*Math.sin(.05*x+dx)+100*Math.cos(.05*x+dx)+40*Math.sin(.05*x+dx),
+    (x,dx) => 100 * Math.sin(.05*x+dx)+300+100*Math.cos(.03*x+dx)+100*Math.sin(.01*x+dx)+40*Math.cos(.05*x+dx),
+];
+
+let curvas = [];
 
 function generarCurvas(){
-    const arrayAmplitud = linSpace(10, 100, 5);
-    const arrayPeriodo = linSpace(0.1, 1, 5);
-    const arrayFase = linSpace(0, 2*Math.PI, 5);
-    const arrayDesplazamiento = linSpace(0, 100, 5);
-    for(let i = 0; i < 5; i++){
-        curvas.push(new Curva(arrayAmplitud[i], arrayPeriodo[i], arrayFase[i], arrayDesplazamiento[i]));
+    curvas = [];
+    for (let i = 0; i < funcionesSinuidales.length; i++) {
+        face+=.01;
+        curvas.push(new Curva(funcionesSinuidales[i],face));
+    }
+}
+
+function dibujarCurvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < curvas.length; i++) {
+        curvas[i].dibujar();
+        curvas[i].incrementarFase();
     }
 }
 
 function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for(let i = 0; i < curvas.length; i++){
-        curvas[i].dibujar();
-    }
+    generarCurvas();
+    dibujarCurvas();
     requestAnimationFrame(animate);
 }
 
-// Genera las curvas y comienza la animación
-generarCurvas();
+// Comienza la animación
 animate();
